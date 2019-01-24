e07f151bbc43b078619eef4ea5fbff30ce0d68c9
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-01-24 18:33:24 +0800

    add basic table styling

--- src/templates/plasticsCategoryTemplate.tsx
+++ src/templates/plasticsCategoryTemplate.tsx
@@ -1,12 +1,33 @@
 import * as React from "react";
 import { LayoutPlastics } from "../components/Layout/LayoutPlastics";
 import { graphql } from "gatsby";
-import { Image } from "../components/Image";
+import { styled, Box } from "primithemes";
+
+const Table = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  & th,
+  td {
+    padding: ${props => props.theme.sizes[2]};
+    background: transparent;
+    text-align: left;
+  }
+  & tr {
+    border: 1px solid;
+    border-left: none;
+    border-right: none;
+    border-color: ${props => props.theme.colors.divider.main};
+  }
+  & tr:nth-child(even) {
+    background: ${props => props.theme.colors.divider.light};
+  }
+`;
 
 interface ProductNode {
   node: {
     title: string;
     image: any;
+    spec: { key: string; value: string }[];
   };
 }
 
@@ -30,7 +51,24 @@ const PlasticsProductCategoryTemplate: React.SFC<Props> = ({
       {products.edges.map(({ node }) => (
         <div key={node.title}>
           <div>{node.title}</div>
-          <Image fluid={node.image} />
+          <Box p={3}>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Key</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                {node.spec.map((sp, i) => (
+                  <tr key={i}>
+                    <td>{sp.key}</td>
+                    <td>{sp.value}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Box>
         </div>
       ))}
     </div>
@@ -48,6 +86,10 @@ export const query = graphql`
       edges {
         node {
           title
+          spec {
+            key
+            value
+          }
           image {
             childImageSharp {
               fluid(maxWidth: 1200) {
